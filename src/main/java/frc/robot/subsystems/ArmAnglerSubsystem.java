36fe518926cdc638e20f002e69cf9d8595eda726
alexkhesin <alex@khesin.com>
2024-04-29 21:35:44 -0400

    - updated/rationalized SmartDashboard names - updated comments - updated member variable naming style (m_ prefix)

--- src/main/java/frc/robot/subsystems/ArmAnglerSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmAnglerSubsystem.java
@@ -24,7 +24,6 @@ import edu.wpi.first.units.Velocity;
 import edu.wpi.first.units.Voltage;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
@@ -81,27 +80,24 @@ import frc.robot.Constants.ArmConstants;
 //   mechanism whose feedforward component is identified using SysId, which is all about voltage)
 
 public class ArmAnglerSubsystem extends ProfiledPIDSubsystem {
-  private final CANSparkMax _motorFollower;
-  private final CANSparkMax _motor;
-  private DutyCycleEncoder absEncoder;
-  private Encoder relEncoder;
-  private RelativeEncoder neoEncoder;
-  private final ArmFeedforward m_feedforward =
-      new ArmFeedforward(
+  private final CANSparkMax m_motorFollower;
+  private final CANSparkMax m_motor;
+  private final DutyCycleEncoder m_absEncoder;
+  private final Encoder m_relEncoder;
+  private final RelativeEncoder m_neoEncoder;
+  private final ArmFeedforward m_feedforward = new ArmFeedforward(
           ArmConstants.kSVolts, ArmConstants.kGVolts,
           ArmConstants.kVVoltSecondPerRad, ArmConstants.kAVoltSecondSquaredPerRad);
 
-  // Mutable holder for unit-safe voltage values, persisted to avoid reallocation.
-  private final MutableMeasure<Voltage> m_appliedVoltage = mutable(Volts.of(0));
-  // Mutable holder for unit-safe linear distance values, persisted to avoid reallocation.
-  private final MutableMeasure<Angle> m_angle = mutable(Rotations.of(0));
-  // Mutable holder for unit-safe linear velocity values, persisted to avoid reallocation.
-  private final MutableMeasure<Velocity<Angle>> m_velocity = mutable(RotationsPerSecond.of(0));
   private final SysIdRoutine m_sysIdRoutine;
 
+  // Mutable holders for unit-safe values, persisted to avoid reallocation.
+  private final MutableMeasure<Voltage> m_appliedVoltage = mutable(Volts.of(0));
+  private final MutableMeasure<Angle> m_angle = mutable(Rotations.of(0));
+  private final MutableMeasure<Velocity<Angle>> m_velocity = mutable(RotationsPerSecond.of(0));
+
   public ArmAnglerSubsystem() {
-    super(
-        new ProfiledPIDController(
+    super(new ProfiledPIDController(
             ArmConstants.kP,
             0,
             0,
@@ -111,27 +107,27 @@ public class ArmAnglerSubsystem extends ProfiledPIDSubsystem {
     // Start pointing up
     setGoal(Units.degreesToRadians(90));
 
-    _motor = new CANSparkMax(Constants.ARMANGLER_MOTOR_LEFT_PORT, MotorType.kBrushless);
-    _motorFollower = new CANSparkMax(Constants.ARMANGLER_MOTOR_RIGHT_PORT, MotorType.kBrushless);
-    _motor.restoreFactoryDefaults();
-    _motorFollower.restoreFactoryDefaults();
+    m_motor = new CANSparkMax(Constants.ARMANGLER_MOTOR_LEFT_PORT, MotorType.kBrushless);
+    m_motorFollower = new CANSparkMax(Constants.ARMANGLER_MOTOR_RIGHT_PORT, MotorType.kBrushless);
+    m_motor.restoreFactoryDefaults();
+    m_motorFollower.restoreFactoryDefaults();
 
-    _motorFollower.follow(_motor, true);
-    absEncoder = new DutyCycleEncoder(Constants.ArmConstants.kDutyEncoderPort);
+    m_motorFollower.follow(m_motor, true);
+    m_absEncoder = new DutyCycleEncoder(Constants.ArmConstants.kDutyEncoderPort);
+    m_relEncoder = new Encoder(Constants.ArmConstants.kEncoderPorts[0],
+                              Constants.ArmConstants.kEncoderPorts[1]);
     // This returns the internal hall sensor whose counts per revolution is 42, very low.
     // Probably not useful because the resolution is so poor.
-    neoEncoder = _motor.getEncoder();
-    relEncoder = new Encoder(Constants.ArmConstants.kEncoderPorts[0],
-                              Constants.ArmConstants.kEncoderPorts[1]);
+    m_neoEncoder = m_motor.getEncoder();
 
     // See the explanation of CPR and PPR at
     // https://docs.wpilib.org/en/stable/docs/hardware/sensors/encoders-hardware.html#quaderature-encoder-resolution
     // For our Through Bore Encoder, Cycles per Revolution is 2048 per http://revrobotics.com/rev-11-1271/
     final int kRevThoroughBoreEncoderPPR = 2048;
     // 1 / PPR is how many rotations per pulse
-    relEncoder.setDistancePerPulse(Units.rotationsToRadians(1.0 / kRevThoroughBoreEncoderPPR));
+    m_relEncoder.setDistancePerPulse(Units.rotationsToRadians(1.0 / kRevThoroughBoreEncoderPPR));
     // Similar, set absolute encoder units to be in radians
-    absEncoder.setDistancePerRotation(Units.rotationsToRadians(1));
+    m_absEncoder.setDistancePerRotation(Units.rotationsToRadians(1));
 
     m_sysIdRoutine =
       new SysIdRoutine(
@@ -140,7 +136,7 @@ public class ArmAnglerSubsystem extends ProfiledPIDSubsystem {
           new SysIdRoutine.Mechanism(
               // Tell SysId how to plumb the driving voltage to the motor(s).
               (Measure<Voltage> volts) -> {
-                _motor.setVoltage(volts.in(Volts));
+                m_motor.setVoltage(volts.in(Volts));
               },
               // Tell SysId how to record a frame of data for each motor on the mechanism being
               // characterized.
@@ -152,15 +148,16 @@ public class ArmAnglerSubsystem extends ProfiledPIDSubsystem {
                         // see the discussion at
                         // https://www.chiefdelphi.com/t/sysid-routine-not-properly-recording-motor-speed/455172
                         m_appliedVoltage.mut_replace(
+                            // TODO: remove this the next line works
                             // _motor.get() * RobotController.getBatteryVoltage(),
-                            _motor.getBusVoltage() * _motor.getAppliedOutput(),
+                            m_motor.getBusVoltage() * m_motor.getAppliedOutput(),
                             Volts))
                     .angularPosition(m_angle.mut_replace(getMeasurement(), Radians))
                     .angularVelocity(
-                        m_velocity.mut_replace(relEncoder.getRate(), RadiansPerSecond));
+                        m_velocity.mut_replace(m_relEncoder.getRate(), RadiansPerSecond));
               },
               // Tell SysId to make generated commands require this subsystem, suffix test state in
-              // WPILog with this subsystem's name ("shooter")
+              // WPILog with this subsystem's name ("ArmAnglerSubsystem")
               this));        
   }
   @Override
@@ -168,58 +165,48 @@ public class ArmAnglerSubsystem extends ProfiledPIDSubsystem {
     // Calculate the feedforward from the setpoint
     double feedforward = m_feedforward.calculate(setpoint.position, setpoint.velocity);
     // Add the feedforward to the PID output to get the motor output
-    _motor.setVoltage(output + feedforward);
+    m_motor.setVoltage(output + feedforward);
   }
 
   @Override
-  // Returns radians
   public double getMeasurement() {
-    return absEncoder.getDistance() + ArmConstants.kArmOffsetRadians;
+    // getDistance returns radians because we set the appropriate setDistancePerRotation
+    // in the constructor.
+    return m_absEncoder.getDistance() + ArmConstants.kArmOffsetRadians;
   }
-  /*
-   * Example command factory method.
-   *
-   * @return a command
-   */
   public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
     return m_sysIdRoutine.quasistatic(direction);
   }
 
-  /**
-   * Returns a command that will execute a dynamic test in the given direction.
-   *
-   * @param direction The direction (forward or reverse) to run the test in
-   */
   public Command sysIdDynamic(SysIdRoutine.Direction direction) {
     return m_sysIdRoutine.dynamic(direction);
   }
 
   public void stop() {
-    _motor.set(0);
+    m_motor.set(0);
   }
 
   public void move(DoubleSupplier s) {
-    _motor.set(s.getAsDouble());
+    m_motor.set(s.getAsDouble());
+  }
+
+  public Command moveArm(DoubleSupplier speed) {
+    return runEnd(() -> { move(speed); },
+                  () -> { stop();});
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     super.periodic();
-    SmartDashboard.putNumber("arm/encoder", absEncoder.getDistance());
-    SmartDashboard.putNumber("arm/adjust_encoder", getMeasurement());
-    SmartDashboard.putNumber("arm/motor", _motor.get());
-    SmartDashboard.putNumber("arm/motor_bus_volt", _motor.getBusVoltage());
-    SmartDashboard.putNumber("arm/motor_applied_output", _motor.getAppliedOutput());
-    SmartDashboard.putNumber("arm/motor_bus_comp", _motor.getVoltageCompensationNominalVoltage());
-    SmartDashboard.putNumber("arm/rate", relEncoder.getRate());
-    SmartDashboard.putNumber("arm/neorate", neoEncoder.getVelocity());
-    SmartDashboard.putNumber("arm/bat_voltage", RobotController.getBatteryVoltage());
-  }
-
-  public Command moveArm(DoubleSupplier speed) {
-    return runEnd(() -> { move(speed); },
-                  () -> { stop();});
+    SmartDashboard.putNumber("arm/encoder/abs/raw", m_absEncoder.getDistance());
+    SmartDashboard.putNumber("arm/encoder/abs/adjusted", getMeasurement());
+    SmartDashboard.putNumber("arm/encoder/relative", m_relEncoder.getRate());
+    SmartDashboard.putNumber("arm/encoder/built-in", m_neoEncoder.getVelocity());
+    SmartDashboard.putNumber("arm/motor/duty_cycle", m_motor.get());
+    SmartDashboard.putNumber("arm/motor/bus_voltage", m_motor.getBusVoltage());
+    SmartDashboard.putNumber("arm/motor/applied_output", m_motor.getAppliedOutput());
+    SmartDashboard.putNumber("arm/motor/voltage_compensation", m_motor.getVoltageCompensationNominalVoltage());
   }
 
   @Override
